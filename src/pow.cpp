a55d84f6e8627fa8a0b7dbce6095ed208be7bd38
mraksoll4 <32709596+mraksoll4@users.noreply.github.com>
2021-04-18 22:38:24 +0300

    Update pow.cpp

--- src/pow.cpp
+++ src/pow.cpp
@@ -94,8 +94,13 @@ unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nF
 
 unsigned int static DarkGravityWave_V1(const CBlockIndex* pindexLast, const Consensus::Params& params) {
     /* current difficulty formula, dash - DarkGravity v3, written by Evan Duffield - evan@dash.org */
+    unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
     const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
     int64_t nPastBlocks = 12;
+	
+		// Diff drop to pow limit solution for 26 block's - we need this for new diff rules.
+    if ((pindexLast->nHeight+1 >= 1857852) && (pindexLast->nHeight+1 < 1857877))
+        return nProofOfWorkLimit;
 	
     // make sure we have at least (nPastBlocks + 1) blocks, otherwise just return powLimit
     if (!pindexLast || pindexLast->nHeight < nPastBlocks) {
@@ -197,10 +202,10 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
  int DiffMode = 1; 
  if (pindexLast->nHeight+1 < 1550011)    { DiffMode = 1; }
  if (pindexLast->nHeight+1 >= 1550011)   { DiffMode = 2; }
- if (pindexLast->nHeight+1 >= 1857852)   { DiffMode = 3; }
+ if (pindexLast->nHeight+1 >= 1857877)   { DiffMode = 3; }
  if (DiffMode == 1) { return GetNextWorkRequired_Legacy(pindexLast, pblock, params); } // legacy litecoin diff
  if (DiffMode == 2) { return DarkGravityWave_V1(pindexLast, params); } // Old variant past block 12 and standart pow limit 00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
- if (DiffMode == 3) { return DarkGravityWave_V2(pindexLast, params); } // New varinant with less pow limit and past block 24 .
+ if (DiffMode == 3) { return DarkGravityWave_V2(pindexLast, params); } // New varinant with less pow limit and past block 24 and pow limit 00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
  return DarkGravityWave_V2(pindexLast, params);
 }
 
