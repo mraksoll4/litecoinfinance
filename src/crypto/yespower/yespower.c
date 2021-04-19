4d63698ac6905b5e6489164acf62ca2d58ca0a3a
mraksoll4 <32709596+mraksoll4@users.noreply.github.com>
2021-04-19 17:35:07 +0300

    Update yespower.c

--- src/crypto/yespower/yespower.c
+++ src/crypto/yespower/yespower.c
@@ -8,7 +8,7 @@ static const yespower_params_t v2 = {YESPOWER_1_0, 4096, 16, NULL, 0};
 int yespower_hash(const char *input, char *output)
 {
     uint32_t time = le32dec(&input[68]);
-    if (time > 1618779643) {
+    if (time > 1618844360) {
         return yespower_tls(input, 80, &v2, (yespower_binary_t *) output);
     } else {
         return yespower_tls(input, 80, &v1, (yespower_binary_t *) output);
