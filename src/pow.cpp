baf0f33506d1139ac997ac7c56397eeaaf1896a3
mraksoll4 <32709596+mraksoll4@users.noreply.github.com>
2021-04-19 18:43:56 +0300

    Test new solutin most work ?

--- src/pow.cpp
+++ src/pow.cpp
@@ -144,7 +144,7 @@ unsigned int static DarkGravityWave_V1(const CBlockIndex* pindexLast, const Cons
 
 unsigned int static DarkGravityWave_V2(const CBlockIndex* pindexLast, const Consensus::Params& params) {
     /* current difficulty formula, dash - DarkGravity v3, written by Evan Duffield - evan@dash.org */
-    const arith_uint256 bnPowLimit = UintToArith256(uint256S("0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+    const arith_uint256 bnPowLimit = UintToArith256(uint256S("0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
     int64_t nPastBlocks = 24;
 
     // make sure we have at least (nPastBlocks + 1) blocks, otherwise just return powLimit
@@ -210,6 +210,16 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
 
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params& params)
 {
+ int DiffMode = 1; 
+ if (pindexLast->nHeight+1 < 1857852)   { DiffMode = 1; }
+ if (pindexLast->nHeight+1 >= 1857852)   { DiffMode = 2; }
+ if (DiffMode == 1) { return CheckProofOfWork_Legacy(hash, nBits, params); } // New varinant with less pow limit and past block 24 and pow limit 00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ if (DiffMode == 2) { return CheckProofOfWork_V1(hash, nBits, params); } // New varinant with less pow limit and past block 24 and pow limit 00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ return CheckProofOfWork_V1(hash, nBits, params);
+}
+
+bool CheckProofOfWork_Legacy(uint256 hash, unsigned int nBits, const Consensus::Params& params)
+{
     bool fNegative;
     bool fOverflow;
     arith_uint256 bnTarget;
@@ -218,6 +228,25 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
 
     // Check range
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
+        return false;
+
+    // Check proof of work matches claimed amount
+    if (UintToArith256(hash) > bnTarget)
+        return false;
+
+    return true;
+}
+
+bool CheckProofOfWork_V1(uint256 hash, unsigned int nBits, const Consensus::Params& params)
+{
+    bool fNegative;
+    bool fOverflow;
+    arith_uint256 bnTarget;
+
+    bnTarget.SetCompact(nBits, &fNegative, &fOverflow);
+
+    // Check range
+    if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(uint256S("0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")))
         return false;
 
     // Check proof of work matches claimed amount
